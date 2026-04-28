2c88089fdbe121ce6c5d2e1d568d9e3690df2786
ralina <garifullinaralina@gmail.com>
2026-04-28 19:32:34 +0300

    refactoring 3 part

--- shared/src/commonMain/kotlin/friends/mobile/core/config/Configuration.kt
+++ shared/src/commonMain/kotlin/friends/mobile/core/config/Configuration.kt
@@ -4,7 +4,7 @@ import io.ktor.http.URLProtocol
 
 data class Configuration(
     val isDebug: Boolean,
-    val baseHost: String = "localhost",
+    val baseHost: String = "10.0.2.2",
     val basePort: Int = 3000,
     val baseProtocol: URLProtocol = URLProtocol.HTTP,
     val settingsName: String = "friends_settings",
