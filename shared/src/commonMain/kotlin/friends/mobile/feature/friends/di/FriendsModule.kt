0a9e9bbf50237d42b62ecfcb706cd6e8e72cb8f8
Danil Zabinskij <rexportydf@mail.ru>
2026-05-11 23:00:04 +0300

    МЕНЯ ЗАЕБАЛ ДЕТЕКТ

--- shared/src/commonMain/kotlin/friends/mobile/feature/friends/di/FriendsModule.kt
+++ shared/src/commonMain/kotlin/friends/mobile/feature/friends/di/FriendsModule.kt
@@ -5,8 +5,8 @@ import friends.mobile.feature.friends.data.remote.FriendsApi
 import friends.mobile.feature.friends.data.repository.FriendsRepositoryImpl
 import friends.mobile.feature.friends.data.usecase.AcceptFriendRequestUseCaseImpl
 import friends.mobile.feature.friends.data.usecase.CancelFriendRequestUseCaseImpl
-import friends.mobile.feature.friends.data.usecase.GetFriendStatusUseCaseImpl
 import friends.mobile.feature.friends.data.usecase.GetFriendsUseCaseImpl
+import friends.mobile.feature.friends.data.usecase.GetFriendStatusUseCaseImpl
 import friends.mobile.feature.friends.data.usecase.GetIncomingFriendRequestsUseCaseImpl
 import friends.mobile.feature.friends.data.usecase.GetOutgoingFriendRequestsUseCaseImpl
 import friends.mobile.feature.friends.data.usecase.RejectFriendRequestUseCaseImpl
@@ -16,8 +16,8 @@ import friends.mobile.feature.friends.data.usecase.SendFriendRequestUseCaseImpl
 import friends.mobile.feature.friends.domain.repository.FriendsRepository
 import friends.mobile.feature.friends.domain.usecase.AcceptFriendRequestUseCase
 import friends.mobile.feature.friends.domain.usecase.CancelFriendRequestUseCase
-import friends.mobile.feature.friends.domain.usecase.GetFriendStatusUseCase
 import friends.mobile.feature.friends.domain.usecase.GetFriendsUseCase
+import friends.mobile.feature.friends.domain.usecase.GetFriendStatusUseCase
 import friends.mobile.feature.friends.domain.usecase.GetIncomingFriendRequestsUseCase
 import friends.mobile.feature.friends.domain.usecase.GetOutgoingFriendRequestsUseCase
 import friends.mobile.feature.friends.domain.usecase.RejectFriendRequestUseCase
