37429c36ab2975d38e9e1045e7d7b8d003bdbf8f
Danil Zabinskij <rexportydf@mail.ru>
2026-05-16 22:50:41 +0300

    fix CI

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -34,9 +34,10 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.compose.material.icons.extended)
-            
+
             // Важно: навигация требует json сериализатор
             implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
 
             implementation(projects.shared)
         }
